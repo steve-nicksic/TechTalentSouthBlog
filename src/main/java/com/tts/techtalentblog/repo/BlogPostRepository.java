a3747c2d9fbb472579d545c00203fa5c788d22ed
steve-nicksic <65583459+steve-nicksic@users.noreply.github.com>
2021-02-12 17:57:19 -0600

    Created Blog Post Repository interface

---
+++ src/main/java/com/tts/techtalentblog/repo/BlogPostRepository.java
@@ -0,0 +1,7 @@
+package com.tts.techtalentblog.repo;
+
+public interface BlogPostRepository {
+}
+
+// we extend the functionality to include the functionality of the Spring CrudRepository, importing the methods needed
+// to modify data in our database.
