1ed9e46a19f6d317979d6dec6b7ccb05dd4985b3
steve-nicksic <65583459+steve-nicksic@users.noreply.github.com>
2021-02-12 10:03:28 -0600

    Created blog post class under a new 'method' package

---
+++ src/main/java/com/tts/techtalentblog/model/BlogPost.java
@@ -0,0 +1,39 @@
+package com.tts.techtalentblog.model;
+
+public class BlogPost {
+
+    private Long id;
+    private String title;
+    private String author;
+    private String blogEntry;
+
+    public BlogPost(String title, String author, String blogEntry) {
+        this.title = title;
+        this.author = author;
+        this.blogEntry = blogEntry;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getBlogEntry() {
+        return blogEntry;
+    }
+
+    public void setBlogEntry(String blogEntry) {
+        this.blogEntry = blogEntry;
+    }
+}
